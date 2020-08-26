438080360d11d84e27ebf8a3a165637eb5e79dba
Вадим Чижик <shakuraz777@gmail.com>
2020-08-27 00:32:41 +0300

    add Xunit.tests

---
+++ InternetShowcase/InternetShowcase.Tests/ProductsControllerTests.cs
@@ -0,0 +1,44 @@
+﻿using InternetShowcase.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace InternetShowcase.Tests
+{
+    public class ProductsControllerTests
+    {
+        [Fact]
+        public void IndexViewDataMessage()
+        {
+            // Arrange
+            ProductsController controller = new ProductsController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.Equal("Hello world!", result?.ViewData["Message"]);
+        }
+
+        [Fact]
+        public void IndexViewResultNotNull()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Fact]
+        public void IndexViewNameEqualIndex()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+            // Assert
+            Assert.Equal("Index", result?.ViewName);
+        }
+    }
+}
