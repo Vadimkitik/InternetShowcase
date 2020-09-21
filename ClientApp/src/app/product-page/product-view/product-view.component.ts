876025345d6afd6ea6ab64e1a2bed74ed705a469
Вадим Чижик <shakuraz777@gmail.com>
2020-09-21 21:19:22 +0300

    loaded productData

--- ClientApp/src/app/product-page/product-view/product-view.component.ts
+++ ClientApp/src/app/product-page/product-view/product-view.component.ts
@@ -3,7 +3,6 @@ import { ActivatedRoute } from '@angular/router';
 
 import { Product } from 'src/app/shared/models/product.model';
 import { ProductService } from 'src/app/shared/services/product.service';
-import { Category } from 'src/app/shared/models/category.model';
 
 @Component({
   selector: 'product-view',
@@ -13,9 +12,9 @@ import { Category } from 'src/app/shared/models/category.model';
 export class ProductViewComponent implements OnInit {
 
   productLine: string;
-  Category: Category[];
   product: Product;
   loaded: boolean = false;
+  oldPrice: boolean = false;
   public errorMsg;
 
   constructor(
@@ -26,12 +25,11 @@ export class ProductViewComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.productLine != null){
       this.productService.getProduct(this.productLine)
           .subscribe((data: Product) => {
             this.product = data;
-            if(this.product.oldPrice != null) this.loaded = true;
+            if(this.product.oldPrice != null) this.oldPrice = true;
+            this.loaded = true;
           }, error => this.errorMsg = error);
-    }
   }
 }
