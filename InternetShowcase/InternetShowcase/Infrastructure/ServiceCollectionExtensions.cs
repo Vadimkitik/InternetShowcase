95424dc383098d61200d491b629b0ee0b467cd93
Vadimkitik <shakuraz777@gmail.com>
2020-11-24 16:57:32 +0300

    cleaning Startup.cs

---
+++ InternetShowcase/InternetShowcase/Infrastructure/ServiceCollectionExtensions.cs
@@ -0,0 +1,101 @@
+﻿using InternetShowcase.Data.Models.Identity;
+using InternetShowcase.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using AutoMapper;
+using InternetShowcase.Data.interfaces;
+using InternetShowcase.Data.Models;
+using InternetShowcase.ViewModels.MappingProfile;
+using InternetShowcase.Data.Repository;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace InternetShowcase.Infrastructure
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services)
+        {
+            services
+                .AddIdentity<User, IdentityRole>(options =>
+                {
+                    options.Password.RequiredLength = 5;
+                    options.Password.RequireDigit = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                .AddEntityFrameworkStores<ShowcaseDbContext>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddJwtAuthentication(
+            this IServiceCollection services, 
+            AppSettings appSettings)
+        {
+            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
+
+            services
+                    .AddAuthentication(opt =>
+                    {
+                        opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                        opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                    })
+                    .AddJwtBearer(options =>
+                    {
+                        options.RequireHttpsMetadata = true;
+                        options.SaveToken = true;
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(key),
+                            ValidateIssuer = false,
+                            ValidateAudience = false
+                        };
+                    });
+
+            return services;
+        }
+
+        public static IServiceCollection AddMappingTransients(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(MappingProfile));
+            services.AddTransient<IAllProducts, ProductReposytory>();
+            services.AddTransient<IRepository<Category>, CategoryRepository>();
+            services.AddTransient<IRepository<UserOld>, UserRepository>();
+
+            return services;
+        }
+
+        public static IServiceCollection AddConfigure(this IServiceCollection services)
+        {
+            services.Configure<FormOptions>(o =>
+            {
+                o.ValueLengthLimit = int.MaxValue;
+                o.MultipartBodyLengthLimit = int.MaxValue;
+                o.MemoryBufferThreshold = int.MaxValue;
+            });
+            return services;
+        }
+
+        public static IServiceCollection AddMyCors(this IServiceCollection services, string MyAllowSpecificOrigins)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(MyAllowSpecificOrigins, builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyHeader()
+                           .AllowAnyMethod()
+                           .AllowCredentials();
+                });
+            });
+            return services;
+        }
+    }
+
+    
+}
--- InternetShowcase/InternetShowcase/Startup.cs
+++ InternetShowcase/InternetShowcase/Startup.cs
@@ -1,25 +1,15 @@
-using AutoMapper;
 using InternetShowcase.Data;
-using InternetShowcase.Data.interfaces;
-using InternetShowcase.Data.Models;
-using InternetShowcase.Data.Models.Identity;
-using InternetShowcase.Data.Repository;
-using InternetShowcase.ViewModels.MappingProfile;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IO;
-using System.Text;
+using InternetShowcase.Infrastructure;
 
 namespace InternetShowcase
 {
@@ -31,80 +21,21 @@ namespace InternetShowcase
         }
 
         readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
-
         public IConfiguration Configuration { get; }
-
        
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddAutoMapper(typeof(MappingProfile));
-            services.AddTransient<IAllProducts, ProductReposytory>();
-            services.AddTransient<IRepository<Category>, CategoryRepository>();
-            services.AddTransient<IRepository<UserOld>, UserRepository>();
-
-            
-            services.AddDbContext<ShowcaseDbContext>(options => options
-                   .UseMySql(Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 11))));
-
             services
-                .AddIdentity<User, IdentityRole>(options => 
-                {
-                    options.Password.RequiredLength = 5;
-                    options.Password.RequireDigit = false;
-                    options.Password.RequireLowercase = false;
-                    options.Password.RequireUppercase = false;
-                    options.Password.RequireNonAlphanumeric = false;
-                })
-                .AddEntityFrameworkStores<ShowcaseDbContext>();
-
-            //!*AUTH
-            var applicationSettingsConfiguration = Configuration.GetSection("ApplicationSettings");
-            services.Configure<AppSettings>(applicationSettingsConfiguration);
-
-            var appSettings = applicationSettingsConfiguration.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services
-                .AddAuthentication(opt =>
-            {
-                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-                .AddJwtBearer(options =>
-             {
-                 options.RequireHttpsMetadata = true;
-                 options.SaveToken = true;
-                 options.TokenValidationParameters = new TokenValidationParameters
-                 {
-                     ValidateIssuerSigningKey = true,
-                     IssuerSigningKey = new SymmetricSecurityKey(key),
-                     ValidateIssuer = false,
-                     ValidateAudience = false
-                 };
-             });
-
-            //*!
-            services.AddControllers().AddNewtonsoftJson(options =>
-                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-            );
-
-            services.Configure<FormOptions>(o =>
-            {
-                o.ValueLengthLimit = int.MaxValue;
-                o.MultipartBodyLengthLimit = int.MaxValue;
-                o.MemoryBufferThreshold = int.MaxValue;
-            });
-            services.AddCors(options =>
-            {
-                options.AddPolicy(MyAllowSpecificOrigins, builder =>
-                {
-                    builder.WithOrigins("http://localhost:4200")
-                           .AllowAnyHeader()
-                           .AllowAnyMethod()
-                           .AllowCredentials();
-                });
-            });
+                .AddMappingTransients()
+                .AddDbContext<ShowcaseDbContext>(options => options
+                   .UseMySql(Configuration.GetConnectionString("DefaultConnection"), new MySqlServerVersion(new Version(8, 0, 11))))
+                .AddIdentity()
+                .AddConfigure()
+                .AddMyCors(MyAllowSpecificOrigins)
+                .AddJwtAuthentication(services.GetApplicationSettings(Configuration))
+                .AddControllers()
+                .AddNewtonsoftJson(options =>
+                     options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
        
@@ -115,21 +46,18 @@ namespace InternetShowcase
                 app.UseDeveloperExceptionPage();
             }
             
-            app.UseRouting();
-            app.UseCors(MyAllowSpecificOrigins);
-
-            app.UseStaticFiles(new StaticFileOptions {
-                FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
-                RequestPath = new PathString("/Resources")
-            });
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.UseRouting()
+               .UseCors(MyAllowSpecificOrigins)
+               .UseStaticFiles(new StaticFileOptions {
+                   FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), @"Resources")),
+                   RequestPath = new PathString("/Resources")
+               })
+               .UseAuthentication()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+               {
+                   endpoints.MapControllers();
+               });
 
             using(var scope = app.ApplicationServices.CreateScope())
             {
