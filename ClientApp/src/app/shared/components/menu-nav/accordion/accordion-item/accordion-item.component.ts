72ad25c239bc46177cee5e68a802130f7e11e7ce
Вадим Чижик <shakuraz777@gmail.com>
2021-01-06 19:44:27 +0300

    add animation to dropList in accordion-item

--- ClientApp/src/app/shared/components/menu-nav/accordion/accordion-item/accordion-item.component.ts
+++ ClientApp/src/app/shared/components/menu-nav/accordion/accordion-item/accordion-item.component.ts
@@ -1,28 +1,52 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, RouterState } from '@angular/router';
 
 @Component({
   selector: 'app-accordion-item',
   templateUrl: './accordion-item.component.html',
-  styleUrls: ['./accordion-item.component.css']
+  styleUrls: ['./accordion-item.component.css'],
+  animations: [
+    trigger('smoothCollapse', [
+      state('initial', style({
+        height: '0',
+        overflow: 'hidden',
+        opacity: '0',
+        visibility: 'hidden'
+      })),
+      state('final', style({
+        overflow: 'hidden'
+      })),
+      transition('initial<=>final', animate('250ms'))
+    ]),
+    trigger('rotatedState', [
+      state('default', style({ transform: 'rotate(0)'})),
+      state('rotated', style({ transform: 'rotate(180deg)'})),
+      transition('default <=> rotated', animate('250ms'))
+    ])
+  ]
 })
 export class AccordionItemComponent implements OnInit {
 
   @Output() closeDrawerEvent = new EventEmitter<boolean>();
-  @Input() data: 
-  { name: string, 
-    line: string, 
-    children: []
-  };
+  @Input() data:
+    {
+      name: string,
+      line: string,
+      children: []
+    };
   showBody = false;
 
-  constructor( private router: Router) {}
+  constructor(private router: Router) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   closeDrawer(value: boolean, line: string) {
     this.closeDrawerEvent.emit(value);
     this.router.navigate(['products/', line]);
+    if (this.showBody == true) {
+      this.toggle();
+    }
   }
 
   toggle() {
