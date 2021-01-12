1ff67cfb7ebde9986bc84fb90ae6a67fc07e2a7d
Вадим Чижик <shakuraz777@gmail.com>
2021-01-12 13:35:48 +0300

    Save FeedbackForm message in DB

--- InternetShowcase/InternetShowcase/Features/SendEmails/SendEmailsService.cs
+++ InternetShowcase/InternetShowcase/Features/SendEmails/SendEmailsService.cs
@@ -1,16 +1,23 @@
-﻿using InternetShowcase.Features.SendEmails.Models;
+﻿using InternetShowcase.Data;
+using InternetShowcase.Features.SendEmails.Models;
+using Microsoft.EntityFrameworkCore;
 using InternetShowcase.Infrastructure.Services;
 using System.Threading.Tasks;
+using InternetShowcase.Data.Models;
 
 namespace InternetShowcase.Features.SendEmails
 {
     public class SendEmailsService : ISendEmailsService
     {
         private readonly IEmailService _emailService;
+        private readonly ShowcaseDbContext _context;
 
-        public SendEmailsService(IEmailService emailService)
+        public SendEmailsService(
+            IEmailService emailService,
+            ShowcaseDbContext context)
         {
             _emailService = emailService;
+            _context = context;
         }
 
         public async Task<Result> SendYourselfEmail(FeedbackFormRequestModel model)
@@ -26,6 +33,21 @@ namespace InternetShowcase.Features.SendEmails
                           $"<p>Название товара: {model.ProductName}</p>" +
                           $"<p>Цена товара на сайте: {model.ProductPrice} р.</p>" +
                           $"<img src=\"{model.ImageUrl}\"/>";
+
+            var product = await _context.Products.FirstOrDefaultAsync(p => p.Name == model.ProductName);
+
+            Question question = new Question
+            {
+                ClientName = model.Name,
+                ClientEmail = model.Email,
+                ClientTelephone = model.Telephone,
+                ClientMessage = model.Message,
+                ProductID = product.Id,
+                Subject = subject
+            }; 
+
+            _context.Questions.Add(question);
+            await _context.SaveChangesAsync();
 
             var result = await _emailService.SendEmailAsync(email, subject, message);
             if (result.Failure)
