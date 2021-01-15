ff8bc8aec3ec8662b6c85a817de2db746a7a5225
Вадим Чижик <shakuraz777@gmail.com>
2021-01-15 14:48:21 +0300

    ng update angular material to 10v

--- ClientApp/src/main.ts
+++ ClientApp/src/main.ts
@@ -1,5 +1,4 @@
 ﻿
-import 'hammerjs';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app/app.module';
