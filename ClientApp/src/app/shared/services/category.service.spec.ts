870e8b2bca8e198e7ad6df03abfb2fb337c7962c
Вадим Чижик <shakuraz777@gmail.com>
2021-01-16 17:51:07 +0300

    add tests to Category.service.spec.ts

--- ClientApp/src/app/shared/services/category.service.spec.ts
+++ ClientApp/src/app/shared/services/category.service.spec.ts
@@ -1,44 +1,91 @@
-import { TestBed } from "@angular/core/testing";
+import { async, TestBed } from "@angular/core/testing";
 
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { MockBackend, MockConnection } from '@angular/';
 import { CategoryService } from "./category.service";
 import { Category } from "../models/category.model";
+import  dataJson  from '../../jsonFiles/dataJson.json';
 
-
+const LOCALCATEGORIES: Category[] = dataJson;
 describe('CategoryService', () => {
 
     let categoryService: CategoryService;
     let httpTestingController: HttpTestingController
-
+    let mockBackend: MockBackend;
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [ HttpClientTestingModule ],
             providers: [CategoryService]
         });
-
         categoryService = TestBed.inject(CategoryService);
         httpTestingController = TestBed.inject(HttpTestingController);
+        mockBackend = getTestBed().get(MockBackend);
     })
 
     it('should retrieve all cagegoryes', () => {
-
-        
         categoryService.getCategories().subscribe((categories: Category[]) => {
-
-            expect(categories).toBeTruthy('No categories returned');
-
+           
+            expect(categories).toBeTruthy();
             expect(categories.length).toBe(50);
 
             const category = categories.find(category => category.id == 1);
-
-            expect(category.name).toBe("Цветы");
+            expect(category.name).toBe("Цветы");            
         });
         
-        const req = httpTestingController.expectOne('api/categories');
+        const req = httpTestingController.expectOne('http://52.174.48.125/api/categories');
+        expect(req.request.method).toEqual("GET");
+        //req.flush({payload: Object.values(LOCALCATEGORIES)});
+        req.flush(LOCALCATEGORIES)
+    });
 
+    it('should find a category by categoryLine', () => {
+        categoryService.getCategory('cveti')
+            .subscribe((category: Category) => {
+
+                expect(category).toBeDefined();
+                expect(category.id).toEqual(1);
+
+            });
+        const req = httpTestingController.expectOne('http://52.174.48.125/api/categories/cveti');
         expect(req.request.method).toEqual("GET");
 
+        req.flush(LOCALCATEGORIES[0]);
+    });
+
+    it('should create a new categoryTest', () => {
+        async(inject([PostService], (service: CategoryService) => {
+            mockBackend.connections.subscribe((connection: MockConnection) => {
+              expect(connection.request.method).toBe(RequestMethod.Post);
+              connection.mockRespond(new Response(new ResponseOptions({})));
+              const contentType = connection.request.headers.get('Content-Type');
+              const status = connection.request.headers.get('Status');
+              expect(contentType).not.toBeNull();
+              expect(contentType).toEqual('application/json');
+            });
+        const category: Category = {
+            id: 1,
+            line: 'testline',
+            name: 'testName',
+            parent_Id: 0,
+            children: null
+        }
+
+        const result = categoryService.createCategory(category);
+        console.log()
+        result.subscribe(
+            (successResult) => {
+                console.log(successResult)
+                expect(successResult).toBeDefined();
+                expect(successResult).toEqual(LOCALCATEGORIES);
+            });
+        const req = httpTestingController.expectOne('http://52.174.48.125/api/categories');
+        expect(req.request.method).toEqual("POST");
+
+      
     });
 
 
-})
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+});
--- ClientApp/src/environments/environment.ts
+++ ClientApp/src/environments/environment.ts
@@ -4,9 +4,10 @@
 
 export const environment = {
   production: false,
-  apiUrl: "api/"
+  apiUrl: "http://52.174.48.125/api/"
 };
-
+//http://52.174.48.125/api/
+//api/
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
