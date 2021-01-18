cc730a9bc06aa34391ab403eb668d1731bb0e9c1
Вадим Чижик <shakuraz777@gmail.com>
2021-01-18 17:18:25 +0300

    edit errorIntersepter.service.spec.ts

--- ClientApp/src/app/shared/services/intersepters/error-intersepter.service.spec.ts
+++ ClientApp/src/app/shared/services/intersepters/error-intersepter.service.spec.ts
@@ -1,13 +1,29 @@
 import { TestBed } from '@angular/core/testing';
+import { IndividualConfig, ToastrService } from 'ngx-toastr/';
 
 import { ErrorIntersepterService } from './error-intersepter.service';
 
 describe('ErrorIntersepterService', () => {
   let service: ErrorIntersepterService;
 
+  const toastrService = {
+    success: (
+      message?: string,
+      title?: string,
+      override?: Partial<IndividualConfig>
+    ) => {},
+    error: (
+      message?: string,
+      title?: string,
+      override?: Partial<IndividualConfig>
+    ) => {},
+  };
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ErrorIntersepterService]
+      providers: [
+        ErrorIntersepterService, 
+        { provide: ToastrService, useValue: toastrService }
+      ]
     });
     service = TestBed.inject(ErrorIntersepterService);
   });
